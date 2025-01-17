7dd3b7caee0dc89e133b119d36c277edf2a50bb1
jiyaski <j.miyagi.hopkins@gmail.com>
2025-01-17 12:41:41 -0500

    profile page: add provider ID and improve styles

--- dww_provider/src/pages/Profile.tsx
+++ dww_provider/src/pages/Profile.tsx
@@ -9,32 +9,51 @@ const Profile = () => {
       <h1>Profile / Settings</h1>
       <div>
         <label>Full Name:</label>
-        <p>Alice Smith</p>
+        <div>
+          <p>Alice Smith</p>
+        </div>
+      </div>
+
+      <div>
+        <label>Provider ID:</label>
+        <div>
+          <p>WQM-U6A</p>
+        </div>
       </div>
 
       <div>
         <label>Email:</label>
-        <p>alice@example.com</p>
-        <a href='#'>Change email</a>
+        <div>
+          <p>alice@example.com</p>
+          <a href='#'>Change email</a>
+        </div>
       </div>
 
       <div>
         <label>Phone:</label>
-        <p>(123) 456-7890</p>
-        <a href='#'>Change phone</a>
+        <div>
+          <p>(123) 456-7890</p>
+          <a href='#'>Change phone</a>
+        </div>
       </div>
 
       <div>
         <label>Password:</label>
-        <p>********</p>
-        <a href='#'>Show password</a>
-        <a href='#'>Change password</a>
+        <div>
+          <div>
+            <p>********</p>
+            <a href='#'>Show password</a>
+          </div>
+          <a href='#'>Change password</a>
+        </div>
       </div>
 
       <div>
         <label>Two-Factor Authentication (2FA):</label>
-        <p>Not set up</p>
-        <a href='#'>Set up 2FA</a>
+        <div>
+          <p>Not set up</p>
+          <a href='#'>Set up 2FA</a>
+        </div>
       </div>
 
       <div>
