d47e333028221d45262af2ce098827a5ece5f85e
Duy Nguyen <dnguyen9074@gmail.com>
2025-02-05 07:58:52 -0500

    Update views.py
    re-added SessionAuthentication

--- dww_backend/api/views.py
+++ dww_backend/api/views.py
@@ -9,7 +9,7 @@ from .serializers import WeightRecordSerializer
 import json
 from rest_framework.authtoken.models import Token
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
-from rest_framework.authentication import TokenAuthentication
+from rest_framework.authentication import TokenAuthentication, SessionAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
