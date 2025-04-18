7fa970749821e6fd65100498cb9acee05135a8ed
jiyaski <j.miyagi.hopkins@gmail.com>
2025-04-18 11:12:40 -0400

    change token expiry from testing values to real-world values

--- dww_backend/core/settings.py
+++ dww_backend/core/settings.py
@@ -85,8 +85,8 @@ REST_FRAMEWORK = {
 
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=2),
-    "REFRESH_TOKEN_LIFETIME": timedelta(minutes=15),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=15),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=14),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
     "ALGORITHM": "HS256",
